3526718ad8f76c62005cbdd92a95c7c474cb572a
NguyenVanPhuc <vanphuctruggc@gmail.com>
2024-12-05 01:21:36 +0700

    navbar

--- src/main/java/nguyenvanphuc/KMAJourey/controller/UserController.java
+++ src/main/java/nguyenvanphuc/KMAJourey/controller/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
 @Slf4j
 @Controller
 public class UserController {
@@ -16,37 +17,40 @@ public class UserController {
     UserService userService;
 
     @GetMapping("/")
-    public String index(){
+    public String index() {
         return "login";
     }
+
     @GetMapping("/register")
-    public String register(){
+    public String register() {
         return "register";
     }
+
     @PostMapping("/login")
-    public String login(@RequestParam("username") String username, Model model){
-        model.addAttribute("username",username);
+    public String login(@RequestParam("username") String username, Model model) {
+        model.addAttribute("username", username);
         return "login";
     }
+
     @GetMapping("/login")
-    public String login(){
+    public String login() {
         return "login";
     }
+
     //Create User
     @PostMapping("/register")
     public String register(@RequestParam("username") String username,
                            @RequestParam("password") String password,
                            @RequestParam("retypePassword") String retypePassword,
                            Model model) {
-        if(userService.checkUser(username)){
-            model.addAttribute("error","Tên người dùng đã tồn tại");
+        if (userService.checkUser(username)) {
+            model.addAttribute("error", "Tên người dùng đã tồn tại");
             return "register";
         }
         if (!(password.equals(retypePassword))) {
             model.addAttribute("errorPassword", "Mật khẩu không trùng khớp");
             return "register";
-        }
-        else {
+        } else {
             User user = userService.createUser(username, password);
             model.addAttribute("username", username);
             return "login";
@@ -57,13 +61,12 @@ public class UserController {
     @PostMapping("/home")
     public String home(@RequestParam("username") String username,
                        @RequestParam("password") String password,
-                       Model model){
-       boolean check = userService.getUser(username,password);
+                       Model model) {
+        boolean check = userService.getUser(username, password);
 
-        if(check){
+        if (check) {
             return "home";
-        }
-        else {
+        } else {
             model.addAttribute("error", "Tên người dùng hoặc mật khẩu không đúng");
             return "login";
         }
